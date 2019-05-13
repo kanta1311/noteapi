e816ef1a6840bc90868b018e4da59c9b9c7febf1
Arvind Kant <kanta1311@gmail.com>
2019-05-13 21:20:56 +0530

    login and sign with slim

--- db.php
+++ db.php
@@ -1,38 +1,62 @@
 <?php
-$dbcon=new mysqli("localhost","root","123456","ionic");
+$dbcon = new mysqli("localhost", "root", "", "ionic");
 
-if($dbcon->connect_error){
-    die("Error found".$dbcon->connect_error);
+if ($dbcon->connect_error) {
+    die("Error found" . $dbcon->connect_error);
 }
 
-function addUser($username,$userpass){
+function addUser($username, $userpass)
+{
     global $dbcon;
 
-    $stmt=$dbcon->prepare("INSERT INTO `user` (`id`, `username`, `password`, `token`) VALUES (NULL, ?, ?, ?)");
+    $stmt = $dbcon->prepare("INSERT INTO `user` (`id`, `username`, `password`, `token`) VALUES (NULL, ?, ?, ?)");
 
-    $stmt->bind_param('sss',$username,$userpass,getToken());
+    $stmt->bind_param('sss', $username, $userpass, getToken());
 
     return $stmt->execute();
-
 }
-function getToken(){
+function getToken()
+{
     return bin2hex(openssl_random_pseudo_bytes(16));
 }
-function getUserAll(){
+function getUserAll()
+{
     global $dbcon;
-    $sql=$dbcon->query('SELECT * FROM `user`');
-    if($sql->num_rows>0){
-        while($row=$sql->fetch_assoc()){
-            $data[]=[
-                'user_id'=>$row['id'],
-                'username'=>$row['username'],
-                'token'=>$row['token'],
+    $sql = $dbcon->query('SELECT * FROM `user`');
+    if ($sql->num_rows > 0) {
+        while ($row = $sql->fetch_assoc()) {
+            $data[] = [
+                'user_id' => $row['id'],
+                'username' => $row['username'],
+                'token' => $row['token'],
             ];
         }
         return $data;
-    }else{
+    } else {
         return null;
     }
 }
 
-echo json_encode(getUserAll());
+function isUserExist($token)
+{
+    global $dbcon;
+    $sql = $dbcon->query("SELECT * FROM `user` WHERE `token`='$token'");
+    return $sql->num_rows > 0;
+}
+
+function login($username,$password){
+    global $dbcon;
+    $sql=$dbcon->query("SELECT * FROM `user` WHERE username='$username' and password='$password'");
+    if ($sql->num_rows > 0) {
+        while ($row = $sql->fetch_assoc()) {
+            $data = [
+                'user_id' => $row['id'],
+                'username' => $row['username'],
+                'token' => $row['token'],
+            ];
+        }
+        return $data;
+    } else {
+        return null;
+    }
+}
--- index.php
+++ index.php
@@ -1,2 +1,64 @@
 <?php
+require 'vendor/autoload.php';
 include('db.php');
+
+$app = new \Slim\App;
+
+$app->post('/login', function ($req, $res) {
+
+    $key = $req->getParsedBody();
+
+    $username = $key['username'];
+    $password = $key['password'];
+
+    if (login($username, $password)) {
+        $data = [
+            'error' => false,
+            'message' => "Login Successful",
+            "data" => login($username, $password)
+        ];
+    } else {
+        $data = [
+            'error' => true,
+            'message' => "Wrong username or password",
+        ];
+    }
+
+    return $res->withJson($data);
+});
+
+$app->post('/signup',function($req,$res){
+    $key = $req->getParsedBody();
+    $username = $key['username'];
+    $password = $key['password'];
+
+    if(empty($username))
+    {
+        $data=[
+            'error'=>true,
+            'message'=>"Username is missing."
+        ];
+    }else if(empty($password)){
+        $data=[
+            'error'=>true,
+            'message'=>"password is missing."
+        ];
+    }
+    else{
+        if(addUser($username,$password)){
+            $data=[
+                'error'=>false,
+                'message'=>"Signup Successful"
+            ];
+        }
+        else{
+            $data=[
+                'error'=>false,
+                'message'=>"User Already Exist. Please login"
+            ];
+        }
+    }
+
+    return $res->withJson($data);
+});
+$app->run();
