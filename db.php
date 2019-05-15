bdc0c5c5b961c823ca8461b1bfd274dfa70064ea
Arvind <arvind@techizer.in>
2019-05-15 19:07:51 +0530

    add and show list of note complete 🏢

--- db.php
+++ db.php
@@ -1,5 +1,5 @@
 <?php
-$dbcon = new mysqli("localhost", "root", "", "ionic");
+$dbcon = new mysqli("localhost", "root", "123456", "ionic");
 
 if ($dbcon->connect_error) {
     die("Error found" . $dbcon->connect_error);
@@ -44,9 +44,10 @@ function isUserExist($token)
     return $sql->num_rows > 0;
 }
 
-function login($username,$password){
+function login($username, $password)
+{
     global $dbcon;
-    $sql=$dbcon->query("SELECT * FROM `user` WHERE username='$username' and password='$password'");
+    $sql = $dbcon->query("SELECT * FROM `user` WHERE username='$username' and password='$password'");
     if ($sql->num_rows > 0) {
         while ($row = $sql->fetch_assoc()) {
             $data = [
@@ -59,4 +60,52 @@ function login($username,$password){
     } else {
         return null;
     }
+}
+
+function getUserDetailsByToken($token)
+{
+    #
+    global $dbcon;
+    $sql = $dbcon->query("SELECT * FROM `user` WHERE token='$token'");
+    if ($sql->num_rows > 0) {
+        while ($row = $sql->fetch_assoc()) {
+            $data = [
+                'user_id' => $row['id'],
+                'username' => $row['username'],
+                'token' => $row['token'],
+            ];
+        }
+        return $data;
+    } else {
+        return null;
+    }
+}
+
+function getUserId($token)
+{
+    return getUserDetailsByToken($token)['user_id'];
+}
+
+function addNote($title, $description, $token)
+{
+    global $dbcon;
+    $sql = $dbcon->prepare("INSERT INTO `note` (`id`, `title`, `description`, `user_id`) VALUES (NULL, ?, ?, ?)");
+    $user_id = getUserId($token);
+    $sql->bind_param('ssi', $title, $description, $user_id);
+    return $sql->execute();
+}
+
+function getAllNotes($token)
+{
+    global $dbcon;
+    $user_id = getUserId($token);
+    $sql = $dbcon->query("SELECT * FROM `note` WHERE user_id=$user_id");
+    while ($row = $sql->fetch_assoc()) {
+        $data[] = [
+            'id' => $row['id'],
+            'title' => $row['title'],
+            'description' => $row['description'],
+        ];
+    }
+    return  $data;
 }
--- index.php
+++ index.php
@@ -27,38 +27,98 @@ $app->post('/login', function ($req, $res) {
     return $res->withJson($data);
 });
 
-$app->post('/signup',function($req,$res){
+$app->post('/signup', function ($req, $res) {
     $key = $req->getParsedBody();
     $username = $key['username'];
     $password = $key['password'];
 
-    if(empty($username))
-    {
-        $data=[
-            'error'=>true,
-            'message'=>"Username is missing."
+    if (empty($username)) {
+        $data = [
+            'error' => true,
+            'message' => "Username is missing."
         ];
-    }else if(empty($password)){
-        $data=[
-            'error'=>true,
-            'message'=>"password is missing."
+    } else if (empty($password)) {
+        $data = [
+            'error' => true,
+            'message' => "password is missing."
         ];
-    }
-    else{
-        if(addUser($username,$password)){
-            $data=[
-                'error'=>false,
-                'message'=>"Signup Successful"
+    } else {
+        if (addUser($username, $password)) {
+            $data = [
+                'error' => false,
+                'message' => "Signup Successful"
             ];
-        }
-        else{
-            $data=[
-                'error'=>false,
-                'message'=>"User Already Exist. Please login"
+        } else {
+            $data = [
+                'error' => false,
+                'message' => "User Already Exist. Please login"
             ];
         }
     }
 
     return $res->withJson($data);
+});
+
+$app->post('/note/add', function ($req, $res) {
+    $key = $req->getParsedBody();
+    $title = $key['title'];
+    $description = $key['description'];
+
+    $token = $req->getHeaders()['HTTP_TOKEN'][0];
+
+    if (empty($token)) {
+        return $res->withJson([
+            'error' => true,
+            'message' => "Token is missing"
+        ]);
+    } else if (empty($title)) {
+        return $res->withJson([
+            'error' => true,
+            'message' => "Title is missing"
+        ]);
+    } else {
+        if (isUserExist($token)) {
+            if (addNote($title, $description, $token)) {
+                return $res->withJson([
+                    'error' => false,
+                    'message' => "Your Note added successfully",
+                    'result'=>getAllNotes($token)
+                ]);
+            } else {
+                return $res->withJson([
+                    'error' => true,
+                    'message' => "Somthing Wrong with us please try again"
+                ]);
+            }
+        } else {
+            return $res->withJson([
+                'error' => true,
+                'message' => "Token not valid"
+            ], 401);
+        }
+    }
+});
+$app->get('/note/list', function ($req, $res) {
+    $token = $req->getHeaders()['HTTP_TOKEN'][0];
+
+    if (empty($token)) {
+        return $res->withJson([
+            'error' => true,
+            'message' => "Token is missing"
+        ]);
+    } else {
+        if (isUserExist($token)) {
+            return $res->withJson([
+                'error' => false,
+                'message' => "All Notes",
+                'result'=>getAllNotes($token)
+            ]);
+        } else {
+            return $res->withJson([
+                'error' => true,
+                'message' => "Token not valid"
+            ], 401);
+        }
+    }
 });
 $app->run();
